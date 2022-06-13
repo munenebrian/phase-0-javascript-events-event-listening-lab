553426e03ca186c3861ebdc4309480d03b2f2417
munenebrian <njerubrianmunene@gmail.com>
2022-06-13 09:03:24 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+function addingEventListener() {}
+const input = document.querySelector('input');
+
 function addingEventListener() {
+    input.addEventListener('pass', function(event) {
+        return "passed"
+    });
 }
